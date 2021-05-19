330efc1e8b19fdc7c88f66b0c89f6ff9d5042f2f
Narciso Lobo <narcisolobo@gmail.com>
2021-05-19 11:40:23 -0500

    started class repo

---
+++ algorithms/w1d2/w1d2.js
@@ -0,0 +1,29 @@
+// JavaScript Variables
+
+// Primitive Datatypes
+// Strings - sequence of characters
+var name = 'narciso'
+
+// Numbers - integers and floating point numbers are included
+var num = 5.6
+var num2 = 10
+
+// Booleans - true or falst
+var isRaining = false
+
+console.log(6 < 10)
+console.log(isRaining)
+
+// For loops
+
+// 1. Sentry
+// 2. Where does the sentry start?
+// 3. Where does the sentry stop?
+// 4. How does the sentry change?
+var sum = 0
+for (var i = 0; i < 10; i++) {
+  console.log(i)
+  // shorthand for sum = sum + i
+  sum += i
+}
+
---
+++ algorithms/w1d3/w1d3.js
@@ -0,0 +1,41 @@
+// Boolean Expressions
+// Boolean Operators
+
+// 5 < 10 = true
+// var i = 0
+// i < 10 = true
+// i++
+// <, >, <=, >=, ==, ===
+
+var num = '5'
+console.log(num == 5)
+
+// Compound conditional operators
+// &&, ||
+
+var isRaining = true
+var day = "Sunday"
+
+if (isRaining == false && day == "Sunday") {
+  console.log("You get to stay in today.")
+} else {
+  console.log("It's raining and you have to go to work.")
+}
+
+// String concatenation
+// Gluing two strings together.
+
+var greeting = "Hello my name is Narciso"
+var myName = "Narciso"
+
+// +=, -=, *=, /=
+
+// Arrays
+// Reference Datatype
+
+// Arrays can hold any datatype
+var myArr = [1, 7, 3, "Four", 5, true, ["Narciso", 1, 2]]
+//           0  1  2  3       4    5   6
+
+console.log(myArr[3].length)
+
